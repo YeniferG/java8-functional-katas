c2ba6b349a7eca0ad34f1037ce37242cee885a35
Yenifer <guajeyenifer@gmail.com>
2021-12-09 12:32:07 -0500

    Realización de la Kata 1 y 2

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -14,9 +15,10 @@ import java.util.Map;
     Output: List of Integers
 */
 public class Kata2 {
-    public static List<Integer> execute() {
-        List<Movie> movies = DataUtil.getMovies();
+    public static List<ImmutableMap<String, Integer>> execute() {
 
-        return ImmutableList.of(1, 2, 3);
+        return DataUtil.getMovies().stream().filter(movie ->
+                movie.getRating() == 5.0).map(movie ->
+                ImmutableMap.of("id", movie.getId())).collect(Collectors.toList());
     }
 }
